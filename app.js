68e33c9ddb24b1aa44d37e5ae864a3df1e094132
Wellington <w_santos1993@outlook.com>
2020-11-16 10:11:48 -0300

    capturando elementes que serão utilizados

---
+++ app.js
@@ -0,0 +1,29 @@
+// capturar
+// os 3 botões
+// os containers de  numeros de placar
+// o container do placar
+
+const paperBubtton = document.getElementById('paperButton');
+const rockButton = document.getElementById('rockButton');
+const scissorsButton = document.getElementById('scissorsButton');
+const scores = document.getElementById('scores');
+const userScore = document.getElementById('userScores');
+const compScore = document.getElementById('compScores');
+const resetButton = document.getElementById('resetButton');
+ 
+/*
+// E se o elemento não tiver id? Use os seletores css!
+const buttonsContainer = document.querySelector("div");
+console.log(buttonsContainer);
+
+
+//imprimindo todo elemento...
+console.log(paperBubtton);
+
+// Capturar um determinado elemento...
+
+console.log(paperBubtton.getAttribute("alt"));
+
+// Alterando um atributo
+// Facam com que o src do paperButton seja o ícone de pedra (./images/rock.pdg)
+paperBubtton.setAttribute("src", "./images/rock.png");*/
